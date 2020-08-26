b3a3697a76a1ec4c4f9fcbc1f1e1fe68c5d9072e
keshav098655 <65311135+keshav098655@users.noreply.github.com>
2020-08-27 02:32:46 +0530

    Initialize Rnamer

---
+++ README.md
@@ -0,0 +1,36 @@
+## Rename Bot )
+---
+
+An Open Source Rename Telegram RoBot
+
+**My Feature**:
+
+👉 Rename any Telegram Files with Permanent Thumbnail Support
+
+### Installation
+
+
+### You can also tap the Deploy To Heroku button below to deploy straight to Heroku!
+
+[![Deploy](https://www.herokucdn.com/deploy/button.svg)](https://www.heroku.com/deploy?)
+
+#### The Hard Way
+
+```sh
+virtualenv -p python3 VENV
+. ./VENV/bin/activate
+pip install -r requirements.txt
+cp sample_config.py config.py
+--- EDIT config.py values appropriately ---
+python bot.py
+```
+
+## Credits, and Thanks to
+
+* [SpEcHlDe](https://telegram.dog/SpEcHlDe) for his [AnyDLBot](https://github.com/SpEcHiDe/AnyDLBot)
+* [Dan Tès](https://telegram.dog/haskell) for his [Pyrogram Library](https://github.com/pyrogram/pyrogram)
+* [Yoily](https://telegram.dog/YoilyL) for his [UploaditBot](https://telegram.dog/UploaditBot)
+
+#### LICENSE
+- GPLv3
+
---
+++ translation.py
@@ -0,0 +1,71 @@
+class Translation(object):
+    START_TEXT = """Hello,This is a Telegram Rename Bot!,
+
+```You can rename any Telegram Document, using this bot! With custom thumbnail```
+
+**Do one By One rename. Otherwise you will get Permenent Ban**
+
+Channel:-© @TG_BotZ
+
+/help for more details.."""
+
+    RENAME_403_ERR = "Sorry. You are not permitted to rename this file."
+    ABS_TEXT = " Please don't be selfish."
+    UPGRADE_TEXT = "<b>👉 Create own Clone Bot.. </b>  /help for Details"
+    DOWNLOAD_START = "**DOWNLOADING 📥**"
+    UPLOAD_START = "**UPLOADING 📤**"
+    RCHD_TG_API_LIMIT = "Downloaded in {} seconds.\nDetected File Size: {}\nSorry. But, I cannot upload files greater than 2 GB due to Telegram API limitations."
+    AFTER_SUCCESSFUL_UPLOAD_MSG = "**Thank you for Using Me Join @TG_BotZ**"
+    AFTER_SUCCESSFUL_UPLOAD_MSG_WITH_TS = "Downloaded in {} seconds.\nUploaded in {} seconds."
+    NOT_AUTH_USER_TEXT = "Please /upgrade your subscription."
+    NOT_AUTH_USER_TEXT_FILE_SIZE = "Detected File Size: {}. Free Users can only upload: {}\nPlease /upgrade your subscription.\nIf you think this is a bug, please contact <a href='https://telegram.dog/ThankTelegram'>@SpEcHlDe</a>"
+    SAVED_CUSTOM_THUMB_NAIL = "**✅ Custom thumbnail saved. This image will be stored permanently**"
+    DEL_ETED_CUSTOM_THUMB_NAIL = "✅ Custom thumbnail cleared succesfully."
+    FF_MPEG_DEL_ETED_CUSTOM_MEDIA = "✅ Media cleared succesfully."
+    SAVED_RECVD_DOC_FILE = "Document Downloaded Successfully."
+    ABOUT_USER ="""
+○ **My Name** : Renamer
+
+○ **Creator** : <a href="https://t.me/lNS4N3">lNS4N3</a> 
+
+○ **Credits** : ```Everyone in this journey```
+
+○ **Language** : Python3
+
+○ **Library** : <a href="https://docs.pyrogram.org/">Pyrogram asyncio 0.16.1</a> 
+
+○ **Source Code** : <a href=" https://t.me/nokkiirunnoippokittum">👉 Click Here</a> 
+
+○ **Server** : <a href="https://herokuapp.com/">Heroku</a> 
+
+○ **Build Status** : v2.1[Beta]""" 
+    PLAN_TEXT = """**Hai Your Plan Details**
+```Plan name: Free User```
+```Expires on: Until my Death 😍```"""
+    CUSTOM_CAPTION_UL_FILE = "**Renamed By @TGRename_Bot**"
+    NO_CUSTOM_THUMB_NAIL_FOUND = "No Custom ThumbNail found."
+    USER_ADDED_TO_DB = "User <a href='tg://user?id={}'>{}</a> added to {} till {}."
+    HELP_USER = """
+ **Hei, Follow These steps..**
+    
+**1.Send file**
+
+**2.Send custom Thumbnail**
+
+**3.Reply the file - /rename New Name.mkv**
+
+ -----------------------------------
+
+ Channel:© @TG_BotZ"""
+
+    REPLY_TO_DOC_FOR_RENAME_FILE = "Reply to a Telegram media to `/rename New Name.extension` with custom thumbnail support.."
+    ABUSIVE_USERS = "You are not allowed to use this bot. If you think this is a mistake, please check /me to remove this restriction."
+    FREE_USER_LIMIT_Q_SZE = """Cannot Process.
+Free users only 1 request per 30 minutes.
+/upgrade or Try 1800 seconds later."""
+    IFLONG_FILE_NAME = """File Name limit allowed by Telegram is {alimit} characters.
+The given file name has {num} characters.
+
+<b>Essays Not allowed in Telegram file name!</b>
+©️ <code>@ReNameBot</code>
+Please short your file name and try again!"""
